0a85698b2f524c27a0816de14224b19e034a4cb3
leehojin-93 <hogenie93@gmail.com>
2021-07-23 23:07:54 +0900

    ${pageContext.request.contextPath} / Repository / Autowired / DataSource

---
+++ src/main/java/com/javaex/dao/GuestbookDao.java
@@ -0,0 +1,154 @@
+package com.javaex.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.javaex.vo.GuestbookVo;
+
+@Repository
+public class GuestbookDao {
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	// field
+	private Connection conn = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
+	
+	// connection method
+	private void getConnection() {
+		try {
+			conn = dataSource.getConnection(); // driver, url, id, pw
+		} catch (SQLException e) {
+			System.out.println("ERROR: " + e);
+		}
+	}
+	
+	// close method
+	private void getClose() {
+		try {
+			if (conn != null)
+				conn.close();
+			if (pstmt != null)
+				pstmt.close();
+			if (rs != null)
+				rs.close();
+		} catch (SQLException e) {
+			System.out.println("ERROR: " + e);
+		}
+	}
+	
+	// INSERT DB
+	public int insert(GuestbookVo g) {
+		int count = -1;
+		
+		getConnection();
+		
+		try {
+			pstmt = conn.prepareStatement(
+					" INSERT INTO "
+					+ " 		guestbook "
+					+ " VALUES ( "
+					+ " 	sqc_no.NEXTVAL, ?, ?, ?, sysdate ) "
+					);
+
+			pstmt.setString(1, g.getName());
+			pstmt.setString(2, g.getPassword());
+			pstmt.setString(3, g.getContent());
+			
+			count = pstmt.executeUpdate();
+			
+			if (count > 0) {
+				System.out.println("[" + g.getName() + "]님이 글을 작성하셨습니다.");
+			} else {
+				System.out.println("ERROR: " + count + " [관리자에게 문의하세요]");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		getClose();
+		
+		return count;
+	}
+	
+	// SELECT DB
+	public List<GuestbookVo> getList() {
+		List<GuestbookVo> guestbookList = new ArrayList<GuestbookVo>();
+		
+		getConnection();
+		
+		try {
+			pstmt = conn.prepareStatement(
+					" SELECT "
+					+ " 	no, "
+					+ " 	name, "
+					+ " 	password, "
+					+ " 	content, "
+					+ " 	reg_date "
+					+ " FROM "
+					+ " 	guestbook "
+					+ " ORDER BY "
+					+ " 	no ASC "
+					);
+			
+			rs = pstmt.executeQuery();
+			
+			while (rs.next()) {
+				guestbookList.add(new GuestbookVo(rs.getInt("no"), rs.getString("name"), rs.getString("password"), rs.getString("content"), rs.getString("reg_date")));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		getClose();
+
+		return guestbookList;
+	}
+	
+	// DELETE DB
+	public int delete(GuestbookVo g) {
+		int count = -1;
+		
+		getConnection();
+		
+		try {
+			pstmt = conn.prepareStatement(
+					" DELETE FROM "
+					+ " 		guestbook "
+					+ " WHERE "
+					+ " 	no = ? "
+					+ " 	AND password = ? "
+					);
+			 
+			pstmt.setInt(1, g.getNo());
+			pstmt.setString(2, g.getPassword());
+			
+			count = pstmt.executeUpdate();
+			
+			if (count > 0) {
+				System.out.println("[" + g.getNo() + "]번 글이 삭제 되었습니다.");
+			} else {
+				System.out.println("ERROR: " + count + " [관리자에게 문의하세요]");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		getClose();
+		
+		return count;
+	}
+	
+}
